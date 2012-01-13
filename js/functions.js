0299a420eb4e8742bf5a8c707203e6cd39dc6d22
LindaChhor <purplelife_16@hotmail.com>
2012-01-13 11:36:07 -0500

    functions with name

--- js/functions.js
+++ js/functions.js
@@ -2,3 +2,15 @@
 /*
 Javascript multi-line comment
 */
+
+//`alert()` pops up a little window with an OK button
+alert('This is an alert.');
+
+// `prompt()` is for user input, has a text field fopr the user to enter some text
+var name = prompt('What is your name?');
+
+//`console.log()`writes out the content of a variable for debugging
+//identical to ActionScript's 'trace()`
+console.log(name);
+
+document.write('Hello, ' + name);
